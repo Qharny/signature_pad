87763d3785c478098b4ff8dc00181b50e9a6c884
Qharny <cannycc1020@gmail.com>
2024-07-28 16:44:12 +0000

    modified

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,39 @@
-import 'package:web/web.dart' as web;
+import 'dart:html';
 
 void main() {
-  final now = DateTime.now();
-  final element = web.document.querySelector('#output') as web.HTMLDivElement;
-  element.text = 'The time is ${now.hour}:${now.minute} '
-      'and your Dart web app is running!';
+  CanvasElement canvas = querySelector('#signatureCanvas') as CanvasElement;
+  CanvasRenderingContext2D ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  
+  bool isDrawing = false;
+  num lastX = 0;
+  num lastY = 0;
+
+  void startDrawing(Event e) {
+    isDrawing = true;
+    MouseEvent mouseEvent = e as MouseEvent;
+    lastX = mouseEvent.offset.x;
+    lastY = mouseEvent.offset.y;
+  }
+
+  void draw(Event e) {
+    if (!isDrawing) return;
+    MouseEvent mouseEvent = e as MouseEvent;
+    
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(mouseEvent.offset.x, mouseEvent.offset.y);
+    ctx.stroke();
+
+    lastX = mouseEvent.offset.x;
+    lastY = mouseEvent.offset.y;
+  }
+
+  void stopDrawing(Event e) {
+    isDrawing = false;
+  }
+
+  canvas.onMouseDown.listen(startDrawing);
+  canvas.onMouseMove.listen(draw);
+  canvas.onMouseUp.listen(stopDrawing);
+  canvas.onMouseLeave.listen(stopDrawing);
 }
